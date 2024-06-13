447f1af223e2b5bb75ddf8d6cae1e6af945d6fe5
Andrea Dolci <dolciandrea614@gmail.com>
2024-06-13 10:19:12 +0200

    cybersecurity

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -1,30 +1,45 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
+import Database from '@ioc:Adonis/Lucid/Database';
+import Hash from '@ioc:Adonis/Core/Hash'
 
 export default class AuthController {
 
   public async login({ request, response, auth }: HttpContextContract) {
     const username = request.input('username')
-    const password = request.input('password')
+    let password = request.input('password')
+    password = await Hash.make(password)
 
-    console.log({ username, password, auth });
+    console.log(`SELECT * FROM users WHERE username = '${username}' AND password = '${password}'`);
+    const users = await Database.rawQuery(
+      `SELECT * FROM users WHERE username = '${username}' AND password = '${password}'`
+    );
 
-    try {
-      const token = await auth.attempt(username, password, {
-        expiresIn: "365day"
-      })
+    if (users && users.length > 0) {
 
-      console.log({ token });
-
-
-      const user = auth.use('api').user
+      console.log(users[0]);
 
       return response.ok({
-        token, user
+        users: users[0]
       })
-
-    } catch (error) {
-      return response.status(400).send(error.message)
     }
+
+    // try {
+    //   const token = await auth.attempt(username, password, {
+    //     expiresIn: "365day"
+    //   })
+
+    //   console.log({ token });
+
+
+    //   // const user = auth.use('api').user
+
+    //   return response.ok({
+    //     token, users
+    //   })
+
+    // } catch (error) {
+    //   return response.status(400).send(error.message)
+    // }
 
   }
 
